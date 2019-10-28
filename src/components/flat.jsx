672ce456b495b786e3bc39ce967ca378b008e636
danko beribak <beribakdanko@gmail.com>
2019-10-28 06:15:37 +0100

    flat image scss

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -13,9 +13,10 @@ class Flat extends Component {
   render() {
 
     return(
-      <div className="card">
-        <div><img src={this.props.src} className="image" /></div>
-        <h4>{this.props.name}</h4>
+      <div className="card" style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('${this.props.src}')`}}>
+
+        <h4 className="name">{this.props.name}</h4>
+        <p className="price">{this.props.price}{this.props.priceCurrency}</p>
       </div>
     );
   }
