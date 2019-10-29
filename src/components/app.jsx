d6664265e9afd2c60118b5fb94fd77c63884812d
danko beribak <beribakdanko@gmail.com>
2019-10-29 04:57:33 +0100

    map and marker created

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import FlatsList from './flats_list';
 import flats from '../../data/flats';
+import Marker from './marker';
+import GoogleMapReact from 'google-map-react';
 
 class App extends Component {
 
@@ -8,7 +10,10 @@ class App extends Component {
     super(props);
 
     this.state = {
-
+      selectedFlat: {
+        lng: 43,
+        lat: 43
+      }
     };
   }
 
@@ -20,7 +25,9 @@ class App extends Component {
           <FlatsList flats={flats} />
         </div>
         <div className="right">
-          <h1>Hello</h1>
+          <GoogleMapReact defaultCenter={{lng: 43, lat: 42.2}} defaultZoom={12} >
+            <Marker lng={this.state.selectedFlat.lng} lat={this.state.selectedFlat.lat} />
+          </GoogleMapReact>
         </div>
       </div>
     );
